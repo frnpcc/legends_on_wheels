1091a88fe529c7b87f9193fc35945efc02e01b1d
daniel vanegas <dfvanegas@gmail.com>
2024-02-27 01:30:41 +0100

    added view and scss for index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_27_140241) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_27_144856) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,9 +80,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_27_140241) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-  add_foreign_key "races", "historical_figures"
-  add_foreign_key "races", "users"
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
+  add_foreign_key "races", "historical_figures"
+  add_foreign_key "races", "users"
 end
