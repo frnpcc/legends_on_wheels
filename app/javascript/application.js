f83798b16806b8fea83de9b08ca719784c4fb949
Francesca Correa <francescacciavaglia@gmail.com>
2024-02-28 15:38:14 +0100

    Resolve Slick Issue & Update Seeds w/ Addresses

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,21 +4,3 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
-import $ from 'jquery'
-import "slick-carousel"
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-
-window.jQuery = $
-window.$ = $
-
-document.addEventListener('turbo:load', () => {
-  $('.carousel-container').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear'
-  });
-});
