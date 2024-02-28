b2bf42ea96a8c18f7aae587984acfdfbefd46d0d
Francesca Correa <francescacciavaglia@gmail.com>
2024-02-28 15:01:46 +0100

    Add Geocoding Methods to Race Model

--- app/models/race.rb
+++ app/models/race.rb
@@ -5,4 +5,7 @@ class Race < ApplicationRecord
   validates :user_id, :historical_figure_id, presence: true
 
   enum car_type: { bmw: 0, volkswagen_bus: 1, ford_mustang: 2 }
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
