01ab9f1995308b5b3328d7f67a7a2e0fa43a47b8
Francesca Correa <francescacciavaglia@gmail.com>
2024-03-01 12:55:08 +0100

    add PgSearchModel to HistoricalFigure Model

--- app/models/historical_figure.rb
+++ app/models/historical_figure.rb
@@ -1,3 +1,10 @@
 class HistoricalFigure < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name__category_and_era,
+    against: [:name, :category, :era],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  }
 end
